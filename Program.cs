47ff6e5958db07fb513009fa529e16ae59ffb643
VIDEN22014 <dxbogdan22014@gmail.com>
2023-04-19 22:44:11 +0300

    rewrite using params

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ Console.OutputEncoding = Encoding.UTF8;
 Random rnd = new Random();
 int[] array1 = new int[rnd.Next(5, 10)];
 int[] array2 = new int[rnd.Next(5, 10)];
+int[] array3 = new int[rnd.Next(5, 10)];
 
 Console.WriteLine("Масив 1:");
 
@@ -13,30 +14,23 @@ for (int i = 0; i < array1.Length; i++)
     array1[i] = rnd.Next(1, 101);
     Console.Write($"{array1[i]} ");
 }
-
 Console.WriteLine("\nМасив 2:");
 for (int i = 0; i < array2.Length; i++)
 {
     array2[i] = rnd.Next(1, 101);
     Console.Write($"{array2[i]} ");
 }
-Console.WriteLine();
-Console.WriteLine();
-
-var tempArray = array1.Union(array2).ToArray();
-
-int[] resultArray = new int[0];
-
-for (int i = 0; i < tempArray.Length; i++)
+Console.WriteLine("\nМасив 3:");
+for (int i = 0; i < array3.Length; i++)
 {
-    if (tempArray[i] % 5 != 0)
-    {
-        Array.Resize(ref resultArray, resultArray.Length + 1);
-        resultArray[resultArray.Length - 1] = tempArray[i];
-    }
+    array3[i] = rnd.Next(1, 101);
+    Console.Write($"{array3[i]} ");
 }
+Console.WriteLine();
+Console.WriteLine();
 
-Array.Sort(resultArray);
+
+int[] resultArray = uniteArrays(array1, array2, array3);
 
 Console.WriteLine("Результуючий масив:");
 for (int i = 0; i < resultArray.Length; i++)
@@ -44,3 +38,20 @@ for (int i = 0; i < resultArray.Length; i++)
     Console.Write($"{resultArray[i]} ");
 }
 Console.WriteLine();
+
+int[] uniteArrays(params int[][] arrays)
+{
+    List<int> tempList = new List<int>();
+    for (int i = 0; i < arrays.Length; i++)
+    {
+        for (int j = 0; j < arrays[i].Length; j++)
+        {
+            if (arrays[i][j] % 5 != 0 && !tempList.Contains(arrays[i][j]))
+            {
+                tempList.Add(arrays[i][j]);
+            }
+        }
+    }
+    tempList.Sort();
+    return tempList.ToArray();
+}
